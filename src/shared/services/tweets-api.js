6621636792bda7489c196cbbc7a17c9a220463e9
Alex <ais_alex@ukr.net>
2023-06-01 14:37:46 +0300

    add api

---
+++ src/shared/services/tweets-api.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://6477b5b0362560649a2ce1ec.mockapi.io/api/',
+});
+
+export const getAllCards = async () => {
+  const { data } = await instance.get('/TweetCard');
+  return data;
+};
+
+export const updateCard = async data => {
+  const { data: result } = await instance.put(`/TweetCard/${id}`, data);
+  return result;
+};
+
+export const deleteContacts = async id => {
+  const { data } = await instance.delete(`/TweetCard/${id}`);
+  return data;
+};
